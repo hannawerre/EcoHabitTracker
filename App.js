0f8b9dacdb69ff8d69ea8a8f27f15a893849b202
hannawerre <hanna.werre.0276@student.uu.se>
2025-04-07 15:45:35 +0200

    Några screens och små ändringar

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    //backgroundColor: '#fff2cf',
+    backgroundColor: '#fff2cf',
     alignItems: 'center',
     justifyContent: 'center',
   },
