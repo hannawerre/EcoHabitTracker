b119d3898ed13b09ba3128d144d629d737334327
hannawerre <hanna.werre.0276@student.uu.se>
2025-04-08 16:55:01 +0200

    navbar implementerad på alla screens samt i App.js

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './app/Screens/HomeScreen'
 import TransportScreen from './app/Screens/TransportScreen'
+import ProfileScreen from './app/Screens/ProfileScreen';
+import CalendarScreen from './app/Screens/CalendarScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,13 +15,43 @@ const MyStack = () => {
         <Stack.Screen
           name="Home" 
           component={HomeScreen}
-          options={{ headerShown: false }} 
+          options={({ navigation }) => {
+            // Hämta den senaste skärmen som användaren var på 
+            const lastRoute = navigation.getState().routes[navigation.getState().index - 1];
+
+            let animation = 'default'; // Standard animation
+            if (lastRoute && lastRoute.name === 'Profile') {
+              animation = 'slide_from_left'; // Kommer från Profile, glid från vänster
+            } else if (lastRoute && lastRoute.name === 'Calendar') {
+              animation = 'slide_from_right'; // Kommer från Calendar, glid från höger
+            }
+
+            return {
+              headerShown: false, 
+              animation: animation, //baserat på den senaste skärmen
+            };
+          }} 
           />
         <Stack.Screen
           name="Transport" 
           component={TransportScreen}
           options={{ headerShown: false }} 
           />
+        <Stack.Screen
+          name="Profile" 
+          component={ProfileScreen}
+          options={{ headerShown: false,
+            animation: 'slide_from_right',
+          }} 
+          />
+        <Stack.Screen
+          name="Calendar" 
+          component={CalendarScreen}
+          options={{ headerShown: false,
+            animation: 'slide_from_left',
+           }} 
+          />
+        {/* Add more screens here */}
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ app/Screens/CalendarScreen.js
@@ -0,0 +1,16 @@
+import { Text, View, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import mainStyles from '../MainStyle';
+import BottomNavBar from '../components/NavigationBar';
+
+export default function HomeScreen() {
+    const navigation = useNavigation();
+
+    return (
+        <View style={mainStyles.container}>
+            <Text> HEJ CalendarScreen </Text>
+            {/* Implementerar navbar */}
+            <BottomNavBar active="Calendar" /> 
+        </View>
+    );
+}
