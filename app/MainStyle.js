61efa2d69a65a1da79088d9f60146104138365ff
Tea <tea.stjarne@gmail.com>
2025-04-08 14:20:29 +0200

    Style script 2.0

---
+++ app/MainStyle.js
@@ -0,0 +1,9 @@
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      backgroundColor: '#fff2cf',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
